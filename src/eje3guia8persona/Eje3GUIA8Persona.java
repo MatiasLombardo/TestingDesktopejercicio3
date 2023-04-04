8c7db82110407a0472cc2a30e2490b28cc06d442
MatiasLombardo <lombardo.matias787@gmail.com>
2023-04-04 17:49:45 -0300

    Inicio ejercicio 3

---
+++ src/eje3guia8persona/Eje3GUIA8Persona.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eje3guia8persona;
+
+
+public class Eje3GUIA8Persona {
+
+    
+    public static void main(String[] args) {
+       
+    }
+
+}
