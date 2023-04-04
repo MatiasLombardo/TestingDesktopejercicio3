31247aec8d09923c62c5519a1ab028373fdd6727
leoarana <127142512+leoarana@users.noreply.github.com>
2023-04-04 17:57:27 -0300

    inicio branch leandro

---
+++ src/entidades/Persona.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*Realizar una clase llamada Persona en el paquete de entidades que tengan los
+siguientes atributos: nombre, edad, sexo (‘H’ para hombre, ‘M’ para mujer, ‘O’
+para otro), peso y altura. Si desea añadir algún otro atributo, puede hacerlo
+. Agregar constructores, getters y setters.*/
+
+package entidades;
+
+/**
+ *
+ * @author leandro
+ */
+public class Persona {
+    
+}
